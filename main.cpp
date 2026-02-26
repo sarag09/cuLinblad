3f44cdd343d482144bd1ecd6a4cc9031627761a1
Sarag <saragsivadasan@gmail.com>
2026-02-26 15:22:10 -0600

    Step 2: PETSc and MPI successfully linked

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <petsc.h>
 
 int main(int argc, char **argv) {
-    std::cout << "cuLindblad: Walking Skeleton Initialized." << std::endl;
+    // 1. Initialize PETSc and MPI
+    PetscCall(PetscInitialize(&argc, &argv, NULL, "Help message for cuLindblad.\n"));
+
+    // 2. Print a message safely across the cluster
+    PetscCall(PetscPrintf(PETSC_COMM_WORLD, "cuLindblad: PETSc Successfully Initialized!\n"));
+
+    // 3. Clean up memory and shut down MPI
+    PetscCall(PetscFinalize());
     return 0;
 }
