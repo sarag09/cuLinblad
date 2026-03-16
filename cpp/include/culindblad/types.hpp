0e184d978000c9d012510f01b138e4525b5b3235
Sarag <saragsivadasan@gmail.com>
2026-03-16 00:11:45 -0500

    Create initial C++ project skeleton and types header

---
+++ cpp/include/culindblad/types.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <complex>
+#include <cstddef>
+
+namespace culindblad {
+
+using Real = double;
+using Complex = std::complex<double>;
+using Index = std::size_t;
+
+} // namespace culindblad
